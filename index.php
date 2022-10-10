99833eebf3acc636bfbb78ad31322a3de85d43cc
Carranya <k.gong82@gmail.com>
2022-10-10 13:55:24 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memory</title>
+    <script type="text/javascript" src="memory.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="header"><h1>Memory</h1></header>
+    <div id="main">
+        <div id="board"></div>
+    </div>
+    <div><p>Turns: <span id="turns">0</span></p></div>
+    <div id="test"></div>
+    <div id="test2"></div>
+</body>
+</html>
+
